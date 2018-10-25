0c6e048978717899e8f7b3a476833e994832525f
Angie Rodday <angie.rodday@gmail.com>
2018-10-25 14:29:52 -0400

    Uploading files via Rstudio

---
+++ download-data.R
@@ -0,0 +1,11 @@
+#to download csv data
+url<-"https://raw.githubusercontent.com/rafalab/dslabs/master/inst/extdata/murders.csv"
+dest_file<-"data/murders.csv"
+download.file(url, destfile=dest_file)
+
+
+#to get R markdown file for murder
+url2<-"https://raw.githubusercontent.com/rairizarry/murders/master/report.Rmd"
+dest_file2<-"report.Rmd"
+download.file(url2, destfile=dest_file2)
+
